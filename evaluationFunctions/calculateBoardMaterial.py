1db86c1c14a154befa1b1c34c03ab6f3b904b62d
sebastianstroyer <sebastian.stroyer@gmail.com>
2025-03-17 08:23:06 +0100

    plan for positional evaluation function

---
+++ evaluationFunctions/calculateBoardMaterial.py
@@ -0,0 +1,4 @@
+# calculate numpy bitboard for each piecetype
+# multiply by piece value (bitboard multiplied with scalar)
+# sum all piece values for each side
+# return difference between white and black
---
+++ evaluationFunctions/calculateDynamicPisitionalMaterial.py
@@ -0,0 +1,4 @@
+# calculate numpy bitboard for each piecetype
+# multiply by positional piece value (ie by another numpy bitboard instead of a scalar)
+# sum all piece values for each side
+# return difference between white and black
