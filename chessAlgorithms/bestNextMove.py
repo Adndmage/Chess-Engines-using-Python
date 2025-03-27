37c3aaff93afee9d7b3bc71b2d93a9c5bada39e9
sebastianstroyer <sebastian.stroyer@gmail.com>
2025-03-27 11:43:45 +0100

    best next move code can no longer effect the board in unintended ways

--- chessAlgorithms/bestNextMove.py
+++ chessAlgorithms/bestNextMove.py
@@ -1,12 +1,12 @@
 def bestNextMovePlayer(board, evaluationFunction):
-    bestMove = None
-    bestEvalf = float('-inf')
-    for move in board.legal_moves:
-        # evaluation function of board after move
-        evalf = evaluationFunction(board.push(move))
-        #undo move
-        board.pop()
-        if evalf > bestEvalf:
-            bestEvalf = evalf
-            bestMove = move
-    return bestMove
+	bestMove = None
+	bestEvalf = float('-inf')
+
+	for move in board.legal_moves:
+		# evaluation function of board after move
+		evalf = evaluationFunction(board.copy().push(move))
+
+		if evalf > bestEvalf:
+			bestEvalf = evalf
+			bestMove = move
+	return bestMove
