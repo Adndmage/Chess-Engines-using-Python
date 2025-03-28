f6184708d657113e125437fdcf71a1e7764d1929
Adndmage <leoleyang@gmail.com>
2025-03-28 11:55:37 +0100

    Added evaluation to material eval function to give high or low eval on checkmates or zero on draws

--- evaluationFunctions/materialValue.py
+++ evaluationFunctions/materialValue.py
@@ -6,10 +6,18 @@ PIECE_VALUES = {
     chess.BISHOP: 35,
     chess.ROOK: 50,
     chess.QUEEN: 90,
-    chess.KING: 0
+    chess.KING: 1000
 }
 
 def calculate_material_value(board):
+    if board.is_game_over():
+        if board.winner == chess.WHITE:
+            return 100000
+        elif board.winner == chess.BLACK:
+            return -100000
+        else:
+            return 0
+
     evaluation = 0
 
     for square, piece in board.piece_map().items():
