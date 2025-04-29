edd0b9c11f8fe2351d86a790f536e65d9ba26514
sebastianstroyer <sebastian.stroyer@gmail.com>
2025-04-29 08:13:12 +0200

    minor bugfix

--- evaluationFunctions/calculateNewInputeTensorAiEvalf.py
+++ evaluationFunctions/calculateNewInputeTensorAiEvalf.py
@@ -1,7 +1,7 @@
 import torch
 import chess
 import numpy as np
-from AI_stuff.TrainAiBoardEvalNN import NewTensorBiggerThanBiggestChessNet, boardToTensor2
+from .AI_stuff.TrainAiBoardEvalNN import NewTensorBiggerThanBiggestChessNet, boardToTensor2
 import os
 
 # Dynamically determine the path to the model file
